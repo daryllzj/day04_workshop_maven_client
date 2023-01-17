263499a2fed94bd847cac19768e14e90c3c12a14
daryllzj <daryllzj@gmail.com>
2023-01-17 16:01:30 +0800

    final commit

--- src/main/java/sdf/App.java
+++ src/main/java/sdf/App.java
@@ -1,5 +1,16 @@
 package sdf;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  * Hello world!
  */
@@ -10,8 +21,43 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws IOException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 12345);
+
+        try (OutputStream os = socket.getOutputStream()){
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            DataOutputStream dos = new DataOutputStream(bos);
+
+            Console cons = System.console(); 
+            String readInput = "", result = "";
+
+            //darryl's code
+            try (InputStream is = socket.getInputStream()) {
+                BufferedInputStream bis = new BufferedInputStream(is);
+                DataInputStream dis = new DataInputStream(bis);
+
+            while (!readInput.equalsIgnoreCase("close")) {
+                readInput = cons.readLine("enter a command ");
+                dos.writeUTF(readInput);
+                dos.flush();
+
+                result = dis.readUTF();
+                System.out.println(result);
+            }
+
+            dis.close();
+            bis.close();
+            is.close();
+            
+        } catch (EOFException e) {
+            socket.close();
+        }
+
+        dos.close();
+        bos.close();
+        os.close();
     }
+}
 }
